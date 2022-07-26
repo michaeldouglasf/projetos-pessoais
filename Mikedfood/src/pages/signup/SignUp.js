ac54837377d49f3b2995cab9b1d2ba9fa26814e3
Michael Douglas <michaeldougls93@gmail.com>
2022-07-26 10:02:34 +0100

    ajuste nas validacoes

--- Mikedfood/src/pages/signup/SignUp.js
+++ Mikedfood/src/pages/signup/SignUp.js
@@ -1,99 +1,97 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useForm } from "../../hooks/useForm";
-import { Main, InputMaterial } from './style';
+import { Main, InputMaterial } from "./style";
 import { IconButton } from "@mui/material";
-import { DivPassword,ButtonStyled } from "./style";
+import { DivPassword, ButtonStyled } from "./style";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
+const SignUp = () => {
+  const { form, onChange, clean } = useForm({
+    name: "",
+    email: "",
+    cpf: "",
+    password: "",
+  });
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showCheckPassword, setShowCheckPass] = useState(false);
+  const [checkErrPass, setCheckErrPass] = useState(false);
 
- const SignUp = () => {
-    const {form,onChange,clean} = useForm({
-        "name":     "",
-        "email":    "",
-        "cpf":      "",
-        "password": ""
-    })
-    const [confirmPassword,setConfirmPasseword] = useState('')
-    const [showPassword,setShowPassword] = useState(false)
-    const [showCheckPassword,setShowCheckPass] = useState(false)
-    const [checkErrPass,setCheckErrPass] = useState(false)
-    
-    const onSubmitForm = (event) =>{
-        event.preventDefault()
-        console.log(form)
-        if(form.password !== confirmPassword) {
-            setCheckErrPass(true)
-        }else{
-            setCheckErrPass(false)
-        }
+  const onSubmitForm = (event) => {
+    event.preventDefault()
+    console.log(form);
+    if (form.password !== confirmPassword) {
+      setCheckErrPass(true);
+    } else {
+      setCheckErrPass(false);
     }
-    const cpfMask=(value)=>{
-        return value
-        .replace(/\D/g,"")
-        .replace(/(\d{3})(\d)/,"$1.$2")
-        .replace(/(\d{3})(\d)/,"$1.$2")
-        .replace(/(\d{3})(\d{1,2})/,"$1-$2")
-        .replace(/(-\d{2})\d+?$/,"$1");
-    }
-    const handleClickShowPassword = () => {
-        setShowPassword(!showPassword)
-      }
-      const handleClickShowCheckPassword = () => {
-        setShowPassword(!showPassword)
-      }
-    return (
-
+  };
+  const cpfMask = (value) => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
+  };
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleClickShowCheckPassword = () => {
+    setShowCheckPass(!showCheckPassword);
+  };
+  return (
     <Main>
-        <p>Cadastrar</p>
-        <form  onSubmit={onSubmitForm}>
-            <InputMaterial
-                id="outlined-basic"
-                label={"Nome"}
-                name='name'
-                type={"text"}
-                placeholder={"Digite seu nome"}
-                variant="outlined"
-                value={form.name}
-                onChange={onChange}
-            />
-
-            <InputMaterial
-                id="outlined-basic"
-                label={"Email"}
-                name='email'
-                type={"text"}
-                placeholder={"Digite seu email"}
-                variant="outlined"
-                value={form.email}
-                onChange={onChange}
-                required
-        /> 
-           <InputMaterial
-                id="outlined-basic"
-                label={"CPF"}
-                name='cpf'
-                type={"text"}
-                placeholder={"Digite seu CPF"}
-                variant="outlined"
-                value={cpfMask(form.cpf)}
-                onChange={onChange}
-                required
+      <p>Cadastrar</p>
+      <form onSubmit={onSubmitForm}>
+        <InputMaterial
+          id="outlined-basic"
+          label={"Nome"}
+          name="name"
+          type={"text"}
+          placeholder={"Digite seu nome"}
+          variant="outlined"
+          value={form.name}
+          onChange={onChange}
         />
-          <DivPassword>
-          <InputMaterial
-             error={checkErrPass}
-             helperText={checkErrPass ?"Deve ser a mesma que anterior" : ''}
-             id="outlined-basic"
-            label="Password"
-            name="password"
-            type={showPassword?'password' :'text'}
-            variant="outlined"
-            placeholder={'Minimo de 6 caracteres'}
-            value= {form.password}
-            onChange={onChange}
-            inputProps={{ minLength: 6,title:"A senha deve conter no minimo 6 caracteres"}}
-             required
 
+        <InputMaterial
+          id="outlined-basic"
+          label={"Email"}
+          name="email"
+          type={"text"}
+          placeholder={"Digite seu email"}
+          variant="outlined"
+          value={form.email}
+          onChange={onChange}
+          required
+        />
+        <InputMaterial
+          id="outlined-basic"
+          label={"CPF"}
+          name="cpf"
+          type={"text"}
+          placeholder={"Digite seu CPF"}
+          variant="outlined"
+          value={cpfMask(form.cpf)}
+          onChange={onChange}
+          required
+        />
+        <DivPassword>
+         <InputMaterial
+            id="outlined-basic"
+            label="Confirmar"
+            name="password"
+            type={showPassword ? "password" : "text"}
+            variant="outlined"
+            placeholder={"Minimo de 6 caracteres"}
+            value={form.password}
+            onChange={onChange}
+            inputProps={{
+              minLength: 6,
+              title: "A senha deve conter no minimo 6 caracteres",
+            }}
+            required
           />
           <IconButton
             aria-label="toggle password visibility"
@@ -104,34 +102,36 @@ import { Visibility, VisibilityOff } from "@mui/icons-material";
           </IconButton>
         </DivPassword>
         <DivPassword>
+         
           <InputMaterial
-             error={checkErrPass}
-             helperText={checkErrPass ? "Deve ser a mesma que anterior" : ''}
-             id="outlined-basic"
+            error={checkErrPass}
+            helperText={checkErrPass ? "Deve ser a mesma que anterior" : ""}
+            id="outlined-basic"
             label="Confirmar"
             name="password"
-            type={showPassword?'password' :'text'}
+            type={showCheckPassword ? "password" : "text"}
             variant="outlined"
-            placeholder={'Minimo de 6 caracteres'}
-            value= {confirmPassword}
-            onChange={onChange}
-            inputProps={{ minLength: 6,title:"A senha deve conter no minimo 6 caracteres"}}
-             required
-
+            placeholder={"Minimo de 6 caracteres"}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            inputProps={{
+              minLength: 6,
+              title: "A senha deve conter no minimo 6 caracteres",
+            }}
+            required
           />
           <IconButton
             aria-label="toggle password visibility"
-            onClick={handleClickShowPassword}
+            onClick={handleClickShowCheckPassword}
             edge="end"
           >
             {showPassword ? <VisibilityOff /> : <Visibility />}
           </IconButton>
         </DivPassword>
-        
+
         <ButtonStyled type="submit">Entrar</ButtonStyled>
-        </form> 
+      </form>
     </Main>
-        
-    )
-}
-export default  SignUp
+  );
+};
+export default SignUp;
